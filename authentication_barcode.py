1aac85a4b363443825fc33f1ce21393f21c6bab9
kabangu_cti <kabangufuture@gmail.com>
2020-09-05 23:45:36 +0200

    autentication

--- authentication_barcode.py
+++ authentication_barcode.py
@@ -7,12 +7,22 @@ cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
+with open('datas.txt') as f:
+    datas_list = f.read().splitlines()
+
+print(datas_list)
+
 while True:
 
     success, img = cap.read()
     for barcode in decode(img):
         mydata = barcode.data.decode('utf-8')
         print(mydata)
+
+        if mydata in datas_list:
+            print("Successful Authenticated")
+        else:
+            print("Failed to authenticate!!!")
 
         # build a shape around the detected barcode.
         pts = np.array([barcode.polygon], np.int32)
